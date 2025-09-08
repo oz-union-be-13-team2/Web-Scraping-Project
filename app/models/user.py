0e50c2c371bd5f9b0c2a12e2c3f7517e2a4cdd97
han-son-03 <rkdgksths11@naver.com>
2025-09-08 17:19:31 +0900

    데이터베이스 비동기 연결

---
+++ app/models/user.py
@@ -0,0 +1,10 @@
+from tortoise import fields, models
+
+class User(models.Model):
+    id = fields.IntField(pk=True)
+    username = fields.CharField(max_length=50, unique=True)
+    email = fields.CharField(max_length=100, unique=True)
+    created_at = fields.DatetimeField(auto_now_add=True)
+
+    class Meta:
+        table = "users"
